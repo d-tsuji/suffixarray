a64aca162c470dd9e6ef73d26905dfbd0c0fd817
d-tsuji <dram.dt.shonan@gmail.com>
2020-06-21 23:55:33 +0900

    add: implement Manber&Myers's building method

--- suffixarray.go
+++ suffixarray.go
@@ -36,6 +36,11 @@ func New(s string) *Manber {
 	return m
 }
 
+func (m *Manber) Build() {
+	m.msd()
+	m.doit()
+}
+
 func (m *Manber) msd() {
 	const R int = 256
 
@@ -61,4 +66,87 @@ func (m *Manber) msd() {
 		m.Index[cumm[m.Text[i]]] = i
 		cumm[m.Text[i]]++
 	}
+}
+
+func (m *Manber) doit() {
+	for m.offset = 1; m.offset < m.N; m.offset += m.offset {
+		var count int
+		for i := 1; i <= m.N; i++ {
+			if m.Rank[m.Index[i]] == m.Rank[m.Index[i-1]] {
+				count++
+			} else if count > 0 {
+				// sort
+				left := i - 1 - count
+				right := i - 1
+				m.quicksort(left, right)
+
+				// now fix up ranks
+				r := m.Rank[m.Index[left]]
+				for j := left + 1; j <= right; j++ {
+					if m.less(m.Index[j-1], m.Index[j]) {
+						r = m.Rank[m.Index[left]] + j - left
+					}
+					m.newrank[m.Index[j]] = r
+				}
+
+				// copy back - note can't update rank too eagerly
+				for j := left + 1; j <= right; j++ {
+					m.Rank[m.Index[j]] = m.newrank[m.Index[j]]
+				}
+
+				count = 0
+			}
+		}
+	}
+}
+
+func (m *Manber) quicksort(lo, hi int) {
+	if hi <= lo {
+		return
+	}
+	i := m.partition(lo, hi)
+	m.quicksort(lo, i-1)
+	m.quicksort(i+1, hi)
+}
+
+func (m *Manber) partition(lo, hi int) int {
+	i, j, v := lo-1, hi, m.Index[hi]
+	for {
+		// find item on left to swap
+		i++
+		for m.less(m.Index[i], v) {
+			if i == hi {
+				break
+			}
+			i++
+		}
+
+		// find item on right to swap
+		j--
+		for m.less(v, m.Index[j]) {
+			if j == lo {
+				break
+			}
+			j--
+		}
+
+		// check if pointers cross
+		if i >= j {
+			break
+		}
+		m.exch(i, j)
+	}
+
+	// swap with partition element
+	m.exch(i, hi)
+
+	return i
+}
+
+func (m *Manber) exch(i, j int) {
+	m.Index[i], m.Index[j] = m.Index[j], m.Index[i]
+}
+
+func (m *Manber) less(v, w int) bool {
+	return m.Rank[v+m.offset] < m.Rank[w+m.offset]
 }
--- suffixarray_test.go
+++ suffixarray_test.go
@@ -3,10 +3,38 @@ package suffixarray
 import (
 	"testing"
 
-	"github.com/google/go-cmp/cmp/cmpopts"
-
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
+
+func TestBuild(t *testing.T) {
+	tests := []struct {
+		name string
+		text string
+		want *Manber
+	}{
+		{
+			name: "normal",
+			text: "abracadabra",
+			want: &Manber{
+				N:     11,
+				Text:  "abracadabra",
+				Index: []int{10, 7, 0, 3, 5, 8, 1, 4, 6, 9, 2, 11 /*sentinels*/},
+				Rank:  []int{2, 6, 10, 3, 7, 4, 8, 1, 5, 9, 0, -1 /*sentinels*/},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := New(tt.text)
+			m.Build()
+			opt := cmpopts.IgnoreUnexported(Manber{})
+			if diff := cmp.Diff(m, tt.want, opt); diff != "" {
+				t.Errorf("m.Build() differs: (-got +want)\n%s", diff)
+			}
+		})
+	}
+}
 
 func TestManber_msd(t *testing.T) {
 	tests := []struct {
